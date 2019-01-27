4b5ee8150a2499a19de0c776452922db7a3070a0
Peterson Oliveira <petersonc.oliveira@gmail.com>
2019-01-27 21:56:06 -0200

    Completed Altimeter class

--- airplane/src/main/scala/avionics/Altimeter.scala
+++ airplane/src/main/scala/avionics/Altimeter.scala
@@ -21,5 +21,21 @@ class Altimeter extends Actor with ActorLogging{
 
     var lastTick = System.concurrentTimeMillis
 
-    
+    val ticker  = context.system.scheduler.schedule(100.millis, 100.millis, self, Tick)
+
+    case object Tick 
+
+    def receive = {
+        case RateChange(amount) => 
+            rateOfClimb = amount.min(1.0f).max(-1.0f) * maxRateOfClimb
+            log info(s"Altimeter changed rate of climb to $rateOfClimb")
+        
+        case Tick => {
+            val tick = System.concurrentTimeMillis
+            altitude = altitude + ((tick - lastTick)) / 60000.0) * rateOfClimb
+            lastTick = tick
+        }
+    }
+
+    override def postStop(): Unit = ticker.cancel
 }
